6636694b7239200c6fdf5cf4ef7540b8044a54e3
Dale <dalestevens1278@gmail.com>
2015-03-23 12:44:51 +0000

    Added frameworks

---
+++ simpleChatApp/simpleChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleChatApp
+//
+//  Created by Dale Stevens on 23/03/2015.
+//  Copyright (c) 2015 Dale Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
